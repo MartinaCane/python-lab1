08ec9cf2528722017d080123723243f6504132a1
MartinaCane <s227716@studenti.polito.it>
2018-03-19 15:52:22 +0100

    hi

---
+++ Ladispe.py
@@ -0,0 +1,23 @@
+#Ex 1
+'''
+print("Type a number")
+n_a= int(input())
+print("Type another number to be added")
+n_b= int(input())
+
+print("The sum of the two numbers ", n_a," and ", n_b, "is ", n_a+n_b )'''
+
+
+#Ex 2
+print("enter a phrase:")
+phrase=input()
+
+len(phrase)
+
+print(phrase[0],phrase[1], phrase[len-2], phrase[len-2])
+
+
+
+
+
+
