47c3d47be61335963ab0bd9e2a19da0651c6e026
MartinaCane <37245339+MartinaCane@users.noreply.github.com>
2018-03-21 14:15:17 +0100

    Update Ladispe.py

--- Ladispe.py
+++ Ladispe.py
@@ -6,7 +6,7 @@ print("Type another number to be added")
 n_b= int(input())
 
 print("The sum of the two numbers ", n_a," and ", n_b, "is ", n_a+n_b )
-'''
+
 
 
 
@@ -21,8 +21,25 @@ print(type(len(phrase)))
 
 print(phrase[0],phrase[1], phrase[len(phrase)-2],phrase[len(phrase)-1])
 
+'''
 
+# Ex 3
 
+print("Insert the number corresponding to the action you want to perform: 1. insert a new task; 2. remove a task; 3. show all the tasks; 4. close the program.")
+print("Your choice: ")
+choice=input()
 
+while choice==1:
+    ask= input()
+    tasks=[ask]
+    print("You want to add more? Y/N")
+    choice2=input()
 
+    if choice2=="Y":
+        print("Go on")
+    tasks.append(input())
+    print(tasks)
 
+        
+else if choice2==N :
+            choice=0
