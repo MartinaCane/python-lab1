f7455a668a73a87c39db53ec2f3d97a3483285dd
MartinaCane <37245339+MartinaCane@users.noreply.github.com>
2018-03-21 13:48:07 +0100

    Update Ladispe.py

--- Ladispe.py
+++ Ladispe.py
@@ -5,16 +5,21 @@ n_a= int(input())
 print("Type another number to be added")
 n_b= int(input())
 
-print("The sum of the two numbers ", n_a," and ", n_b, "is ", n_a+n_b )'''
+print("The sum of the two numbers ", n_a," and ", n_b, "is ", n_a+n_b )
+'''
 
 
-#Ex 2
+
+
+# Ex 2
 print("enter a phrase:")
 phrase=input()
 
-len(phrase)
+print("The lenght of the phrase is ",len(phrase))
 
-print(phrase[0],phrase[1], phrase[len-2], phrase[len-2])
+print(type(len(phrase)))
+
+print(phrase[0],phrase[1], phrase[len(phrase)-2],phrase[len(phrase)-1])
 
 
 
